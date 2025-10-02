84724d4e63d8fe9cc89808177c801307c0d1fa91
Omer Ahmed <ayahyam18@gmail.com>
2025-10-02 10:24:03 +0300

    Added ORM queries for Author and Book

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,5 @@
-from relationship_app.models import Author, Book, Library, Librarian
-
-# 1. Query all books by a specific author
-def books_by_author(author_name):
-    return Book.objects.filter(author__name=author_name)
-
-# 2. List all books in a library
-def books_in_library(library_name):
-    return Library.objects.get(name=library_name).books.all()
-
-# 3. Retrieve the librarian for a library
-def librarian_for_library(library_name):
-    return Library.objects.get(name=library_name).librarian
+from .models import Author, Book
+def get_author_by_name(author_name):
+    return Author.objects.get(name=author_name)
+def get_books_by_author(author):
+    return Book.objects.filter(author=author)
