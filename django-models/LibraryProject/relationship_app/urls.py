c39168acbacdd4b4d338ddcacfbc2643b2744b27
Omer Ahmed <ayahyam18@gmail.com>
2025-10-03 15:02:55 +0300

    D

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
+from django.contrib.auth.views import LoginView,LogoutView
+from django.urls import path
 
 urlpatterns = [
     path("library_detail/",LibraryDetailView.as_view(),name="library detail"),
     path("list_books/",list_books,name="list books"),
+    path('login/', LoginView.as_view(template_name='relationship_app//login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Library
 from .models import Book
-from django.views.generic.detail import DetailView
+from django.views.generic.detail import DetailView 
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
+class MODEL_NAMECreateView(CreateView):
+    model = UserCreationForm
+    template_name = "relationship_app/register.html"
+    success_url=reverse_lazy('login')
+
 def list_books(request):
     books=Book.objects.all()
     return render(request,'relationship_app/list_books.html',{'books':books})
