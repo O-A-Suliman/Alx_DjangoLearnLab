d395020a209fb66210d7e89db1e460a8301f6ba3
Omer Ahmed <ayahyam18@gmail.com>
2025-10-03 22:28:15 +0300

    D

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("list_books/",views.list_books,name="list books"),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/',views.register, name='register')
+    path('register/',views.register, name='register'),
+    path('admin-dashboard/', views.admin_view, name='admin_view'),
+    path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
+    path('member-dashboard/', views.member_view, name='member_view'),
 ]
