ba96c02681839afbf8e5f91e8e5648dcc5104f3a
Omer Ahmed <ayahyam18@gmail.com>
2025-10-11 18:48:06 +0300

    D

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -2,10 +2,29 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Book
+from .serializers import BookSerializer
+
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  
+    permission_classes = [permissions.AllowAny]
+
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+    search_fields = ['title', 'author__name']
+
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title'] 
 
 
 class BookDetailView(generics.RetrieveAPIView):
